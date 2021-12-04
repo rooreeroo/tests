d3821238424a43331378bcc5fb2fae455b2e4dc2
rooreeroo <rureeru@gmail.com>
2021-12-04 19:34:41 +0300

    upgrade v2

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -8,26 +8,25 @@ import plural from "plural-ru";
 
 
 function Controls({card, sum, counter}){
-  console.log(card, );
   const [modalActive, setModalActive] = useState(false);
   return (
   <div>
     <div className='Controls'>В корзине:
-      <div className="card"> {   // вот сюда хочу вывести общую стоимость и количество
-
+      <div className="card"> {
 
         card.length === 0 ?
             'пусто' :
             (counter + ' ' +
-            plural(counter, 'товар', 'товара', 'товаров') + ' / ' +
-            sum.toString(10).replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ') + ' ₽ ')
+                plural(counter, 'товар', 'товара', 'товаров') + ' / ' +
+                sum.toString(10).replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ') + ' ₽ ')
       }
       </div>
       <button onClick={() => setModalActive(true)}> Показать</button>
     </div>
     <Basket active={modalActive} setActive={setModalActive} card={card} sum={sum} counter={counter}> </Basket>
   </div>
-  )}
+  )
+}
 
 Controls.propTypes = {
 }
