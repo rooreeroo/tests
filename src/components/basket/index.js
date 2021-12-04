07f6182e7a582c73de01717fce6df81d6adf32a8
rooreeroo <rureeru@gmail.com>
2021-12-04 19:27:31 +0300

    upgrade

--- src/components/basket/index.js
+++ src/components/basket/index.js
@@ -1,14 +1,33 @@
 import React from "react";
 import propTypes from 'prop-types';
 import './styles.css';
+import Card from "../card";
 
-const Basket = ({active, setActive}) => {
+const Basket = ({active, setActive, card, sum, counter}) => {
+    console.log('items',card);
     return (
         <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
             <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
-                <div className="modal__content-card">
-                    <h1>Корзина</h1>
-                    <button className="btn-close" onClick={() => setActive(false)}>Закрыть</button>
+                <div className="modal__content-header">
+                    <div className="modal__content-card">
+                        <h1>Корзина</h1>
+                        <button className="btn-close" onClick={() => setActive(false)}>Закрыть</button>
+                    </div>
+                </div>
+                <div className='List-items-card'>{card.map(item =>
+                    <div className='List__item-card' key={item.code}>
+                        <Card code={item.code}
+                              title={item.title}
+                              price={item.price}
+                              counter={item.counter}
+                              basket={card}/>
+                    </div>
+                )}
+                </div>
+                <div className="card-full-price">
+                    <div className="full">Итого</div>
+                    <div className="full-price">{sum.toString(10).replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ') + ' ₽ '} </div>
+                    <div className="full-counter">{counter + ' ' + 'шт'}</div>
                 </div>
             </div>
         </div>
--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -2,29 +2,30 @@ import React, {useState} from "react";
 import propTypes from 'prop-types';
 import './styles.css';
 import Basket from "../basket";
+import plural from "plural-ru";
 
 
 
 
-function Controls(card, fullPrice){
-  console.log(card, fullPrice);
+function Controls({card, sum, counter}){
+  console.log(card, );
   const [modalActive, setModalActive] = useState(false);
   return (
   <div>
     <div className='Controls'>В корзине:
-      <div className="card"> { console.log(fullPrice)   // вот сюда хочу вывести общую стоимость и количество
+      <div className="card"> {   // вот сюда хочу вывести общую стоимость и количество
 
 
-        // store.state.basket.counter === 0 ?
-        //     'пусто' :
-        //     (store.state.basket.counter + ' ' +
-        //     plural(store.state.basket.counter, 'товар', 'товара', 'товаров') + ' / ' +
-        //     store.state.basket.price.toString(10).replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ') + ' ₽ ')
+        card.length === 0 ?
+            'пусто' :
+            (counter + ' ' +
+            plural(counter, 'товар', 'товара', 'товаров') + ' / ' +
+            sum.toString(10).replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ') + ' ₽ ')
       }
       </div>
       <button onClick={() => setModalActive(true)}> Показать</button>
     </div>
-    <Basket active={modalActive} setActive={setModalActive}> </Basket>
+    <Basket active={modalActive} setActive={setModalActive} card={card} sum={sum} counter={counter}> </Basket>
   </div>
   )}
 
